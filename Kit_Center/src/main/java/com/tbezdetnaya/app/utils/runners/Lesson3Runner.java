b6f1250902d05c49b95afc7d7905fdb85327f047
TBezdetnaya <aburamushi4@mail.ru>
2016-11-11 11:51:53 +0200

    homework_lesson3

---
+++ Kit_Center/src/main/java/com/tbezdetnaya/app/utils/runners/Lesson3Runner.java
@@ -0,0 +1,20 @@
+package com.tbezdetnaya.app.utils.runners;
+
+import com.tbezdetnaya.app.utils.convertors.PrimitiveConvertor;
+
+/**
+ * Created by Tanya on 10.11.2016.
+ */
+public class Lesson3Runner {
+    public static void main(String[] args) {
+        PrimitiveConvertor convertor = new PrimitiveConvertor();
+        convertor.floatToChar();
+        convertor.intToChar();
+        convertor.charToInt();
+
+
+
+
+
+    }
+}
