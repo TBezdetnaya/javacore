36a4d7abfc1f95163ab56efffe7bda3282fa76c1
Tanya <aburamushi4@mail.ru>
2016-11-23 12:04:16 +0200

    correction according to comments_v2

---
+++ src/main/java/com/tbezdetnaya/runners/Lesson5Runner.java
@@ -0,0 +1,13 @@
+package com.tbezdetnaya.runners;
+
+import com.tbezdetnaya.app.homework.lesson5.Menu;
+
+/**
+ * Created by ACER PC on 23.11.2016.
+ */
+public class Lesson5Runner {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.startMenu();
+    }
+}
