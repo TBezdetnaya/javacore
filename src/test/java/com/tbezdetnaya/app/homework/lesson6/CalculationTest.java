8e50447ae5b09cfd4a1fad1d7257c1ab991cd627
Tanya <aburamushi4@mail.ru>
2016-11-23 14:52:47 +0200

    edit test

--- src/test/java/com/tbezdetnaya/app/homework/lesson6/CalculationTest.java
+++ src/test/java/com/tbezdetnaya/app/homework/lesson6/CalculationTest.java
@@ -16,12 +16,27 @@ public class CalculationTest {
     public void testCalculatorCirclArea( double expected, double radius) throws Exception {
         Assert.assertEquals(expected, calculation.calculatorCirclArea(radius),0.001);
 
+
     }
+
+    @Test
+    @FileParameters(value = "src/test/resources/FileData_1.1.csv", mapper = CsvWithHeaderMapper.class)
+    public void testCalculatorCirclArea01( double expected, double radius) throws Exception {
+        Assert.assertNotEquals(expected, calculation.calculatorCirclArea(radius),0.001);
+
+    }
+
 
     @Test
     @FileParameters(value = "src/test/resources/FileData_3.csv", mapper = CsvWithHeaderMapper.class)
     public void testIsEvenOrOdd(double a) throws Exception {
         Assert.assertTrue(calculation.isEvenOrOdd(a));
+    }
+
+    @Test
+    @FileParameters(value = "src/test/resources/FileData_3.1.csv", mapper = CsvWithHeaderMapper.class)
+    public void testIsEvenOrOdd01(double a) throws Exception {
+        Assert.assertNotEquals(calculation.isEvenOrOdd(a), 0.001);
 
 
     }
@@ -31,6 +46,11 @@ public class CalculationTest {
     public void testEqualsNumber(double a, double b) throws Exception {
         Assert.assertTrue("Numbers are equal", calculation.equalsNumber(a, b));
 
+    }
+    @Test
+    @FileParameters(value = "src/test/resources/FileData_2.1.csv", mapper = CsvWithHeaderMapper.class)
+    public void testEqualsNumber01(double a, double b) throws Exception {
+        Assert.assertNotEquals(calculation.equalsNumber(a, b), 0.001);
 
     }
 
@@ -40,4 +60,12 @@ public class CalculationTest {
         Assert.assertTrue(calculation.angledTriangle(a, b, c));
 
     }
+    @Test
+    @FileParameters(value = "src/test/resources/FileData_4.1.csv", mapper = CsvWithHeaderMapper.class)
+    public void testAngledTriangle01(double a, double b, double c) throws Exception {
+        Assert.assertNotEquals(calculation.angledTriangle(a, b, c), 0.001);
+
+    }
+
+
 }
