2c0423690c91d8ed281ee7c84094e04ff5336824
Tania <aburamushi@mail.ru>
2016-12-05 08:29:05 +0200

    lesson9 homework_v2

--- src/main/java/com/tbezdetnaya/runners/homework/Lesson9Runner.java
+++ src/main/java/com/tbezdetnaya/runners/homework/Lesson9Runner.java
@@ -14,58 +14,54 @@ public class Lesson9Runner {
         LoopArrays loopArrays = new LoopArrays();
         ArrayPrint arrayPrint = new ArrayPrint();
         Scanner sc = new Scanner(System.in);
-        try{
-            do{
-                System.out.println("Привет! данное приложение выполняет несколько задач, выбирете число");
-                System.out.println("----------------------------------------------------");
-                System.out.println("1 - для cоздания массива из всех чётных чисел от 2 до 20");
-                System.out.println("2 - для создания из всех не чётных чисел от 1 до 99");
-                System.out.println("3 - для создания массива из 15 случайных целых чисел из отрезка [0;9]");
-                System.out.println("4 - для создания массива из 15 случайных целых чисел из отрезка [0;999]");
-                System.out.println("5 - для вывода двумерного массива из 8 строк по 5 столбцов в каждой из случайных целых чисел из отрезка [10;99]");
-                System.out.println("6 - для вывода двумерного массив из 8 строк по 5 столбцов в каждой из случайных целых чисел из отрезка [1;999]");
-                System.out.println("7 - для выполнения задачи №7");
-                System.out.println("8 или 9 - для сортировки массива из случайных чисел по возростанию.");
-                System.out.println("10 или 11 - для сортировки массива из случыйных чисел по убыванию.");
-                System.out.println("12 - для сортировки выбором, массива из случайных чисел.");
-                System.out.println(">");
-                try{
-                    switch (sc.nextInt()){
-                        case  1: loopArrays.showDimentionalArray();
-                            break;
-                        case 2: loopArrays.sortDimentionalArray();
-                            break;
-                        case  3: loopArrays.arrayEvenNumbers();
-                            break;
-                        case 4: loopArrays.arrayMinMaxNumbers();
-                            break;
-                        case 5: loopArrays.sortMultiDimentionalArray();
-                            break;
-                        case 6: loopArrays.sortMultiDimentionalArrayNew();
-                            break;
-                        case 7: loopArrays.sumOfumbers();
-                            break;
-                        case 8: arrayPrint.printResultSortArrayAscend();
-                            break;
-                        case 9: arrayPrint.printResultArraySortAscend();
-                            break;
-                        case 10: arrayPrint.printResultArraySortDecrease();
-                            break;
-                        case 11: arrayPrint.printResultSortArrayDecrease();
-                            break;
-                        case 12: arrayPrint.printResultSelectionSortArray();
-                            break;
 
-                    }
+        do {
+            System.out.println("Привет! данное приложение выполняет несколько задач, выбирете число");
+            System.out.println("----------------------------------------------------");
+            System.out.println("1 - для cоздания массива из всех чётных чисел от 2 до 20");
+            System.out.println("2 - для создания из всех не чётных чисел от 1 до 99");
+            System.out.println("3 - для создания массива из 15 случайных целых чисел из отрезка [0;9]");
+            System.out.println("4 - для создания массива из 15 случайных целых чисел из отрезка [0;999]");
+            System.out.println("5 - для вывода двумерного массива из 8 строк по 5 столбцов в каждой из случайных целых чисел из отрезка [10;99]");
+            System.out.println("6 - для вывода двумерного массив из 8 строк по 5 столбцов в каждой из случайных целых чисел из отрезка [1;999]");
+            System.out.println("7 - для выполнения задачи №7");
+            System.out.println("8 или 9 - для сортировки массива из случайных чисел по возростанию.");
+            System.out.println("10 или 11 - для сортировки массива из случыйных чисел по убыванию.");
+            System.out.println("12 - для сортировки выбором, массива из случайных чисел.");
+            System.out.println(">");
 
-                }catch (Exception e){
+            switch (sc.nextInt()) {
+                case 1: loopArrays.showDimentionalArray();
+                    break;
+                case 2: loopArrays.sortDimentionalArray();
+                    break;
+                case 3: loopArrays.arrayEvenNumbers();
+                    break;
+                case 4: loopArrays.arrayMinMaxNumbers();
+                    break;
+                case 5: loopArrays.sortMultiDimentionalArray();
+                    break;
+                case 6: loopArrays.sortMultiDimentionalArrayNew();
+                    break;
+                case 7: loopArrays.sumOfumbers();
+                    break;
+                case 8: arrayPrint.printResultSortArrayAscend();
+                    break;
+                case 9: arrayPrint.printResultArraySortAscend();
+                    break;
+                case 10: arrayPrint.printResultArraySortDecrease();
+                    break;
+                case 11: arrayPrint.printResultSortArrayDecrease();
+                    break;
+                case 12: arrayPrint.printResultSelectionSortArray();
+                    break;
+                default:
                     System.out.println("Error: invalide value");
-                }
-            }while (Validation.needContinue());
 
-        }catch (Exception e){
-            System.out.println("Error: invalide value");
-        }
+            }
+        } while (Validation.needContinue());
+
+
 
     }
 }
