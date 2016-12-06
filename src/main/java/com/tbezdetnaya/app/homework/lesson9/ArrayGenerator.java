5396cb35c3adcde85349465ab05240e55db1172c
Tanya <aburamushi4@mail.ru>
2016-12-06 11:16:05 +0200

    lesson9 homework_v5

--- src/main/java/com/tbezdetnaya/app/homework/lesson9/ArrayGenerator.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson9/ArrayGenerator.java
@@ -4,9 +4,12 @@ package com.tbezdetnaya.app.homework.lesson9;
  * Created by tanya on 05.12.2016.
  */
 public class ArrayGenerator {
+   private Integer n = 10;
+   private int a = 5;
+   private int b = 8;
 
     public int[][] createMultiDimentionalArray(){
-        int arr[][] = new int[5][8];
+        int arr[][] = new int[a][b];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 arr[i][j] = (int) (Math.random() * 199) - 99;
@@ -15,7 +18,7 @@ public class ArrayGenerator {
         return arr;
     }
     public Integer[] creatRandomArray (){
-        Integer arr[] = new Integer[10];
+        Integer arr[] = new Integer[n];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 100);
         }
