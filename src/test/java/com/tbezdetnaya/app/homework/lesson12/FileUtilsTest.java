192f5157d6ae21498572f4b591add53fd7c27f09
Tania <aburamushi@mail.ru>
2016-12-25 12:49:03 +0200

    lesson 12 homework_Junit

--- src/test/java/com/tbezdetnaya/app/homework/lesson12/FileUtilsTest.java
+++ src/test/java/com/tbezdetnaya/app/homework/lesson12/FileUtilsTest.java
@@ -3,7 +3,9 @@ package com.tbezdetnaya.app.homework.lesson12;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 import static org.junit.Assert.*;
 
@@ -12,12 +14,16 @@ import static org.junit.Assert.*;
  */
 public class FileUtilsTest {
     FileUtils fileUtils = new FileUtils();
+    private String arrayData = "resources/standard.txt";
+    private String writeFileData = "resources/ArrayWrite.txt";
+    private  double [] arr = {12.0, 4.0, 67.8, 6.0, 45.0, 5.0, 2.3, 1.0, 0.0};
+    private double [] expected = {12, 4, 67.8, 6, 45, 5, 2.3, 1, 0};
+   private String [] expectedS = {"moom", "word", "noon", "ere"};
 
 
 
     @Test
     public void testReadFileNumber() throws Exception {
-        double [] expected = {12, 4, 67.8, 6, 45, 5, 2.3, 1, 0};
         double[] result = fileUtils.readFileNumber();
         Assert.assertArrayEquals(expected,result,0);
 
@@ -26,11 +32,22 @@ public class FileUtilsTest {
 
     @Test
     public void testReadFileWord() throws Exception {
-        String [] expected = {"moom", "word", "noon", "ere"};
+
         String [] result = fileUtils.readFileWord();
-        Assert.assertArrayEquals(expected,result);
+        Assert.assertArrayEquals(expectedS,result);
 
     }
 
 
+    @Test
+    public void testWriteFileNumber() throws Exception {
+
+        fileUtils.writeFileNumber(arr);
+        BufferedReader readStandard = new BufferedReader(new FileReader(arrayData));
+        String expected = readStandard.readLine();
+        BufferedReader readResult = new BufferedReader(new FileReader(writeFileData));
+        String result = readResult.readLine();
+        Assert.assertEquals(expected.toString(),result.toString());
+
+    }
 }
