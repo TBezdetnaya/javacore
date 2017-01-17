5b27bfe53519418e409c71a82d3392da22921268
Tanya <aburamushi4@mail.ru>
2017-01-17 16:49:59 +0200

    lesson 16_homework_v3

--- src/main/java/com/tbezdetnaya/app/homework/lesson16/PersonUtils.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson16/PersonUtils.java
@@ -1,18 +1,21 @@
 package com.tbezdetnaya.app.homework.lesson16;
+
 import com.tbezdetnaya.app.homework.lesson7.Validation;
+
 import java.util.Random;
 import java.util.Scanner;
+
 /**
  * Created by tanya on 16.01.2017.
  * Реализовать игру “Угадай-ка”, которая будет выводить случайным образом ФИО одного из известных людей и предлагать пользователю ввести его возраст.
  * В случае совпадения - выводим пользователю поздравление, в противном случае - сообщаем что он неправ и выводим случайным образом ФИО другого известного человека.
  * ФИО не должно повторяться два раза подряд.
- Задачу можно усложнить, спрашивая у пользователя не только возраст, а также то - жив ли этот человек или уже нет. Игра должна завершаться после трех угаданных людей.
- Список людей с ФИО, их возрастами и статусами жизни должен находится в enum.
+ * Задачу можно усложнить, спрашивая у пользователя не только возраст, а также то - жив ли этот человек или уже нет. Игра должна завершаться после трех угаданных людей.
+ * Список людей с ФИО, их возрастами и статусами жизни должен находится в enum.
  */
 public class PersonUtils {
     private Random randomGenerator;
-    private  Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
     private int count = 0;
 
     public PersonUtils() {
@@ -26,30 +29,27 @@ public class PersonUtils {
     }
 
     public void guessPersonData() {
-        do {
-            try {
-                do {
-                    Person p = getRandomPerson();
-                    System.out.println("How old is " + p.getFullName() + "?");
-                    String inputAge = sc.next();
-                    System.out.println("This person is 'alive' or 'died'?");
-                    String inputString = sc.next();
-                    int age = new Integer(inputAge);
-                    if (p.getAge() == age && p.isAlive().equals(inputString)) {
-                        System.out.println("Congratulations, you guessed!");
-                        break;
-                    } else {
-                        count++;
-                    }
-                } while (count < 3);
-                if (count == 3) {
-                    System.out.println("You did not guess age and 'alive' or 'died'!");
-                }
 
-            } catch (Exception e) {
-                System.out.println("Error: invalid value");
+        do {
+            Person p = getRandomPerson();
+            System.out.println("How old is " + p.getFullName() + "?");
+            String inputAge = sc.next();
+            System.out.println("This person is 'alive' or 'died'?");
+            String inputString = sc.next();
+            int age = new Integer(inputAge);
+
+            if (p.getAge() == age && p.isAlive().equals(inputString)) {
+                System.out.println("Congratulations, you guessed!");
+                break;
+            } else {
+                count++;
             }
-        }while (Validation.needContinue());
+
+        } while (count < 3);
+        if (count == 3) {
+            System.out.println("You did not guess age and 'alive' or 'died'!");
+        }
+
 
     }
 }
--- src/main/java/com/tbezdetnaya/runners/homework/Lesson16Runner.java
+++ src/main/java/com/tbezdetnaya/runners/homework/Lesson16Runner.java
@@ -1,6 +1,8 @@
 package com.tbezdetnaya.runners.homework;
 import com.tbezdetnaya.app.homework.lesson16.MenuItem;
 import com.tbezdetnaya.app.homework.lesson16.PersonUtils;
+import com.tbezdetnaya.app.homework.lesson7.Validation;
+
 import java.util.Scanner;
 
 /**
@@ -11,16 +13,18 @@ public class Lesson16Runner {
         PersonUtils utils = new PersonUtils();
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the game of \"Guess the age of the famous person\". Enter text \"TASK[number]\" to begin the work:");
-            try {
-                MenuItem item = MenuItem.valueOf((sc.nextLine().toUpperCase()));
-                switch (item){
-                    case TASK1:
-                        utils.guessPersonData();
-                        break;
-                }
-            } catch (Exception e) {
-                System.out.println("Error: invalid value");
+
+        try {
+            MenuItem item = MenuItem.valueOf((sc.nextLine().toUpperCase()));
+            switch (item) {
+                case TASK1:
+                    utils.guessPersonData();
+                    break;
             }
+        } catch (Exception e) {
+            System.out.println("Error: invalid value");
+        }
+
 
     }
 }
