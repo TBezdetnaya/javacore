1160c98fd65bafe4a0d76c8718b4454c067c3de4
Tanya <aburamushi4@mail.ru>
2017-01-18 19:07:22 +0200

    lesson17_v3_classwork

--- src/main/java/com/tbezdetnaya/app/classWork/lesson17/ArraySorterGenerics.java
+++ src/main/java/com/tbezdetnaya/app/classWork/lesson17/ArraySorterGenerics.java
@@ -1,17 +1,22 @@
 package com.tbezdetnaya.app.classWork.lesson17;
 
-import java.util.Arrays;
+
 
 /**
  * Created by ACER PC on 17.01.2017.
  */
-public class ArraySorterGenerics<T> {
+public class ArraySorterGenerics<U extends Number> {
+    private U []arr;
+    public ArraySorterGenerics(U[]arr) {
+        this.arr = arr;
 
-    public <U extends Comparable<U>> U[] arrayBubbleSortAscending(U[] arr) {
+    }
+
+    public <U extends Comparable  > U[] arrayBubbleSortAscending(U[] arr) {
         U temp;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
-                if ( arr[j + 1].compareTo(arr[j])>0) {
+                if ( arr[j + 1].compareTo(arr[j])<0) {
                      temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -19,9 +24,10 @@ public class ArraySorterGenerics<T> {
                 }
             }
         }
-        // System.out.println("Sorted array:" + Arrays.toString(arr));
+
         return arr;
     }
+
 
 
 }
--- src/main/java/com/tbezdetnaya/app/classWork/lesson17/Main.java
+++ src/main/java/com/tbezdetnaya/app/classWork/lesson17/Main.java
@@ -7,11 +7,20 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        ArraySorterGenerics arr = new ArraySorterGenerics();
+
         Integer[] arrayInteger = {2, 10, 72, 2, 9, 40, 48, 5, 5, 49};
-        Float[] arrFloat = {2f, 10f, 72f, 2f, 9f, 40f, 48f, 5f, 5f, 49f };
-        System.out.println(Arrays.toString(arr.arrayBubbleSortAscending(arrayInteger)));
-        System.out.println(Arrays.toString(arr.arrayBubbleSortAscending(arrFloat)));
+        Float[] arrF = {2f, 10f, 72f, 2f, 9f, 40f, 48f, 5f, 5f, 49f };
+        Double[] arrD = {34.0, 45.6, 4.6, 7.0, 56.7};
+        ArraySorterGenerics <Integer> arrInt = new ArraySorterGenerics<>(arrayInteger);
+        ArraySorterGenerics <Float> arrFloat = new ArraySorterGenerics<>(arrF);
+        ArraySorterGenerics <Double> arrDouble = new ArraySorterGenerics<>(arrD);
+
+
+        System.out.println(Arrays.toString(arrInt.arrayBubbleSortAscending(arrayInteger)));
+        System.out.println(Arrays.toString(arrFloat.arrayBubbleSortAscending(arrF)));
+        System.out.println(Arrays.toString(arrDouble.arrayBubbleSortAscending(arrD)));
+
+
 
         BoxPrinter<Integer> value1 = new BoxPrinter<>(new Integer(10));
 
