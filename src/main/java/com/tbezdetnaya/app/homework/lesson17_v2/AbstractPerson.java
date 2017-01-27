aa2930fd43df0a09cefcbb6755c933a190cfdf56
Tanya <aburamushi4@mail.ru>
2017-01-27 23:27:03 +0200

    lesson17

--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/AbstractPerson.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/AbstractPerson.java
@@ -4,8 +4,52 @@ package com.tbezdetnaya.app.homework.lesson17_v2;
  * Created by Tanya on 27.01.2017.
  */
 public abstract class AbstractPerson {
-    String name;
-    String surname;
-    String faculty;
+    private String name;
+    private String surname;
+    private String faculty;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractPerson that = (AbstractPerson) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (surname != null ? !surname.equals(that.surname) : that.surname != null) return false;
+        return faculty != null ? faculty.equals(that.faculty) : that.faculty == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (faculty != null ? faculty.hashCode() : 0);
+        return result;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
 }
--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/Student.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/Student.java
@@ -4,4 +4,5 @@ package com.tbezdetnaya.app.homework.lesson17_v2;
  * Created by Tanya on 27.01.2017.
  */
 public class Student extends AbstractPerson {
+    private String course;
 }
