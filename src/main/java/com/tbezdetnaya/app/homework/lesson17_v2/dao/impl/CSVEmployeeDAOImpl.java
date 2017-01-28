ffef6d224cb301f478174513d143172c35d8685e
Mushi <kurai0410@gmail.com>
2017-01-28 21:07:04 +0200

    lesson 17

--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/dao/impl/CSVEmployeeDAOImpl.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/dao/impl/CSVEmployeeDAOImpl.java
@@ -1,7 +1,14 @@
 package com.tbezdetnaya.app.homework.lesson17_v2.dao.impl;
 import com.tbezdetnaya.app.homework.lesson17_v2.dao.EmployeeDAO;
+import com.tbezdetnaya.app.homework.lesson17_v2.storage.CSVStorage;
+
 /**
  * Created by Tanya on 28.01.2017.
  */
 public class CSVEmployeeDAOImpl implements EmployeeDAO {
+    private final CSVStorage storage;
+
+    public CSVEmployeeDAOImpl(CSVStorage storage) {
+        this.storage = storage;
+    }
 }
--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/dao/impl/CSVSturentDAOImpl.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/dao/impl/CSVSturentDAOImpl.java
@@ -1,9 +1,14 @@
 package com.tbezdetnaya.app.homework.lesson17_v2.dao.impl;
 
 import com.tbezdetnaya.app.homework.lesson17_v2.dao.StudentDAO;
+import com.tbezdetnaya.app.homework.lesson17_v2.storage.CSVStorage;
 
 /**
  * Created by Tanya on 28.01.2017.
  */
 public class CSVSturentDAOImpl implements StudentDAO {
+    private final CSVStorage storage;
+    public CSVSturentDAOImpl(CSVStorage storage) {
+        this.storage = storage;
+    }
 }
