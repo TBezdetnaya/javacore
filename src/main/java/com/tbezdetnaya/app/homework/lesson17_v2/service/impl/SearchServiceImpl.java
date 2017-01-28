9f83881a4f8db816ee9acf506df42abd5a4bea85
Mushi <kurai0410@gmail.com>
2017-01-28 22:16:25 +0200

    implements SearchService

--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/service/impl/SearchServiceImpl.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/service/impl/SearchServiceImpl.java
@@ -2,11 +2,12 @@ package com.tbezdetnaya.app.homework.lesson17_v2.service.impl;
 
 import com.tbezdetnaya.app.homework.lesson17_v2.dao.EmployeeDAO;
 import com.tbezdetnaya.app.homework.lesson17_v2.dao.StudentDAO;
+import com.tbezdetnaya.app.homework.lesson17_v2.service.SearchService;
 
 /**
  * Created by Tanya on 28.01.2017.
  */
-public class SearchServiceImpl {
+public class SearchServiceImpl implements SearchService {
     private final StudentDAO studentDAO;
     private final EmployeeDAO employeeDAO;
 
