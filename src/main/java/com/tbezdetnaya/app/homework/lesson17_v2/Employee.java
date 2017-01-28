ddfdfc386fe7093b62ad5bf110bcbee9c89ccf05
Mushi <kurai0410@gmail.com>
2017-01-28 09:31:33 +0200

    lesson17_v2

--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/Employee.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/Employee.java
@@ -4,6 +4,7 @@ package com.tbezdetnaya.app.homework.lesson17_v2;
  * Created by Tanya on 27.01.2017.
  */
 public class Employee extends AbstractPerson {
+    private EmployeePosition position;
 
 
 }
--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/Student.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/Student.java
@@ -5,4 +5,7 @@ package com.tbezdetnaya.app.homework.lesson17_v2;
  */
 public class Student extends AbstractPerson {
     private String course;
+    private StudentType type;
+
+
 }
