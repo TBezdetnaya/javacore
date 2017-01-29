b04d731151e35d7ca445ef9955b1f81fe00c1da8
Mushi <kurai0410@gmail.com>
2017-01-29 19:58:25 +0200

    lesson17

--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/Main.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/Main.java
@@ -1,18 +1,33 @@
 package com.tbezdetnaya.app.homework.lesson17_v2;
 
 
+
+import com.tbezdetnaya.app.homework.lesson17_v2.dao.impl.CSVEmployeeDAOImpl;
+import com.tbezdetnaya.app.homework.lesson17_v2.dao.impl.CSVStudentDAOImpl;
+import com.tbezdetnaya.app.homework.lesson17_v2.service.impl.SearchServiceImpl;
 import com.tbezdetnaya.app.homework.lesson17_v2.storage.CSVStorage;
 
 import java.io.IOException;
+import java.util.Scanner;
+
 
 /**
  * Created by Tanya on 28.01.2017.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+
         CSVStorage csvStorage = new CSVStorage("resources/cardindex.csv");
-        csvStorage.getEmployees();
-        csvStorage.getStudents();
+        CSVStudentDAOImpl csvStudentDAO = new CSVStudentDAOImpl(csvStorage);
+        CSVEmployeeDAOImpl csvEmployeeDAO = new CSVEmployeeDAOImpl(csvStorage);
+        SearchServiceImpl searchService = new SearchServiceImpl(csvStudentDAO,csvEmployeeDAO);
+        System.out.println("Enter one of the values: name or surname");
+        searchService.searchSurnameOrName(sc.next());
+
+
+
 
     }
 }
--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/service/impl/SearchServiceImpl.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/service/impl/SearchServiceImpl.java
@@ -3,10 +3,13 @@ package com.tbezdetnaya.app.homework.lesson17_v2.service.impl;
 import com.tbezdetnaya.app.homework.lesson17_v2.dao.EmployeeDAO;
 import com.tbezdetnaya.app.homework.lesson17_v2.dao.StudentDAO;
 import com.tbezdetnaya.app.homework.lesson17_v2.domain.AbstractPerson;
+import com.tbezdetnaya.app.homework.lesson17_v2.domain.Employee;
+import com.tbezdetnaya.app.homework.lesson17_v2.domain.Student;
 import com.tbezdetnaya.app.homework.lesson17_v2.service.SearchService;
 
-
+import java.util.ArrayList;
 import java.util.List;
+
 
 /**
  * Created by Tanya on 28.01.2017.
@@ -21,11 +24,28 @@ public class SearchServiceImpl implements SearchService {
 
     }
 
+
     @Override
     public List<AbstractPerson> searchSurnameOrName(String input) {
 
+        List<Student> students = studentDAO.getStudents();
+        List<Employee> employees = employeeDAO.getEmployees();
+        List<AbstractPerson> result = new ArrayList<>();
+        for (int i = 0; i <students.size() ; i++) {
+            Student student = students.get(i);
+            if (student.getName().toLowerCase().contains(input)
+                    ||student.getSurname().toLowerCase().contains(input)){
+                result.add(student);
+            }
+        }
+        for (int i = 0; i <employees.size() ; i++) {
+            Employee employee = employees.get(i);
+            if(employee.getName().toLowerCase().contains(input)
+                    ||employee.getSurname().toLowerCase().contains(input)){
+                result.add(employee);
+            }
 
-
-        return null;
+        }
+        return result;
     }
 }
