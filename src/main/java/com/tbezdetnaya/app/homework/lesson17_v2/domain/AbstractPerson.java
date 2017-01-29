23a0f19a8334a81d5dc320a2fe7cc44d1a55cfae
Mushi <kurai0410@gmail.com>
2017-01-29 20:43:17 +0200

    lesson17

--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/domain/AbstractPerson.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/domain/AbstractPerson.java
@@ -7,6 +7,15 @@ public abstract class AbstractPerson {
     private String name;
     private String surname;
     private String faculty;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     private int id;
 
 
