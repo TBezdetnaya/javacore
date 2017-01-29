934e385b6a81fc9f13e3453bac59caf1beed6624
Mushi <kurai0410@gmail.com>
2017-01-30 00:10:46 +0200

    lesson17

--- src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/controller/ApplicationController.java
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/controller/ApplicationController.java
@@ -1,9 +1,12 @@
 package com.tbezdetnaya.app.homework.lesson17_v2.controller;
 
+import com.tbezdetnaya.app.homework.lesson17_v2.domain.AbstractPerson;
+
 import com.tbezdetnaya.app.homework.lesson17_v2.service.SearchService;
 
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -19,16 +22,18 @@ public class ApplicationController {
 
         Scanner sc = new Scanner(System.in);
 
+
         try{
             System.out.println("Choose search category:");
-            System.out.println("SEARCH_PERSONS");
+            System.out.println("SEARCH_PERSON");
             System.out.println("SEARCH_EMPLOYEE");
             System.out.println("SEARCH_STUDENT");
             MenuItem item = MenuItem.valueOf((sc.next().toUpperCase()));
             switch (item){
                 case SEARCH_PERSON:
                     System.out.println("Enter one of the values: name or surname");
-                    searchService.searchPersonsBySurnameOrName(sc.next());
+                    List <AbstractPerson> persons = searchService.searchPersonsBySurnameOrName(sc.next());
+                    printPersonsBySurnameOrName(persons);
                     break;
                 case SEARCH_EMPLOYEES:
                     System.out.println("Enter one of the values: name,surname, or faculty");
@@ -48,6 +53,16 @@ public class ApplicationController {
 
 
 
-    private void printPersonsBySurnameOrName() {
+   private void printPersonsBySurnameOrName(List<AbstractPerson> persons) {
+        if(persons.isEmpty()) {
+            System.out.println("Not found");
+        }else{
+            for (final AbstractPerson person : persons) {
+                System.out.println(person.getName()+ " " + person.getSurname());
+            }
+        }
+
+
+
     }
 }
