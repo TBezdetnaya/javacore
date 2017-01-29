4b07f96b96ae3035f750b0b6055b2405f2166a9e
Mushi <kurai0410@gmail.com>
2017-01-29 22:12:07 +0200

    lesson17

---
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/controller/MenuController.java
@@ -0,0 +1,16 @@
+package com.tbezdetnaya.app.homework.lesson17_v2.controller;
+
+/**
+ * Created by Tanya on 29.01.2017.
+ */
+public class MenuController {
+    public void personsSearch(){
+
+    }
+    public void studentsSearch(){
+
+    }
+    public void employeeSearch(){
+        
+    }
+}
