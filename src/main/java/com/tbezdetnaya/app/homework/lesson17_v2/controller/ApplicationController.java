50ac21bf468328f36e1aa3a63fd3a93a28bfca40
Mushi <kurai0410@gmail.com>
2017-01-29 22:23:56 +0200

    lesson17

---
+++ src/main/java/com/tbezdetnaya/app/homework/lesson17_v2/controller/ApplicationController.java
@@ -0,0 +1,26 @@
+package com.tbezdetnaya.app.homework.lesson17_v2.controller;
+
+import com.tbezdetnaya.app.homework.lesson17_v2.service.SearchService;
+
+/**
+ * Created by Tanya on 29.01.2017.
+ */
+public class ApplicationController {
+    public ApplicationController(SearchService searchService) {
+    }
+    public void start(){
+        
+    }
+
+    public void personsSearch(){
+
+    }
+    public void studentsSearch(){
+
+    }
+    public void employeeSearch(){
+
+    }
+    private void printPersonsBySurnameOrName() {
+    }
+}
