50c200a4741ea502ec705c0c04e19b7738c0a399
Mushi <kurai0410@gmail.com>
2017-01-31 11:07:48 +0200

    lesson17_test_StudentReader

--- src/test/java/com/tbezdetnaya/app/homework/lesson17/cardIndex/storage/reader/StudentReaderTest.java
+++ src/test/java/com/tbezdetnaya/app/homework/lesson17/cardIndex/storage/reader/StudentReaderTest.java
@@ -1,5 +1,7 @@
 package com.tbezdetnaya.app.homework.lesson17.cardIndex.storage.reader;
 import com.tbezdetnaya.app.homework.lesson17.cardIndex.domain.Student;
+import com.tbezdetnaya.app.homework.lesson17.cardIndex.domain.StudentType;
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -8,12 +10,20 @@ import org.junit.Test;
  * Created by tanya on 30.01.2017.
  */
 public class StudentReaderTest {
+
     @Test
     public  void readTest (){
         StudentReader studentReader = new StudentReader();
+        Student studentCompare = new Student();
         String[] data = new String[]{"1", "Ivan", "Ivanov", "engineering", "2", "BACHELOR", "STUDENT"};
         Student student = studentReader.read(data);
-        
+        studentCompare.setId(1);
+        studentCompare.setName("Ivan");
+        studentCompare.setSurname("Ivanov");
+        studentCompare.setFaculty("engineering");
+        studentCompare.setCourse(2);
+        studentCompare.setType(StudentType.BACHELOR);
+        Assert.assertEquals(studentCompare,student);
 
 
     }
